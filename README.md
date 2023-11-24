fe24ddd427e87381efc041880341f4d2ed2279d6
Just Coding <jzhu@github.com>
2023-11-24 13:50:18 -0500

    inital set up

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ spark-in-Java
 
 in windows environment, download winutils.exe
 https://github.com/steveloughran/winutils
+create folder C:\hadoop\bin
--- src/main/java/WordsCount.java
+++ src/main/java/WordsCount.java
@@ -4,6 +4,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 public class WordsCount {
     public static void main(String[] args) {
+        System.setProperty("hadoop.home.dir", "C:\\hadoop");
         SparkConf conf = new SparkConf().setAppName("spark-in-Java").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> initialRdd = sc.textFile("src/main/resources/sparkDev.txt");
