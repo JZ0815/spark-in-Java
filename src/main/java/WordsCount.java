32b0b4454df20c4f3a6b565bb98f75511d4a2b7b
Just Coding <jzhu@github.com>
2023-11-24 14:10:55 -0500

    counting words

--- src/main/java/WordsCount.java
+++ src/main/java/WordsCount.java
@@ -1,13 +1,20 @@
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
 
 public class WordsCount {
     public static void main(String[] args) {
         System.setProperty("hadoop.home.dir", "C:\\hadoop");
         SparkConf conf = new SparkConf().setAppName("spark-in-Java").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> initialRdd = sc.textFile("src/main/resources/sparkDev.txt");
+        JavaRDD<String> lineRdd = sc.textFile("src/main/resources/sparkDev.txt");
+        lineRdd.flatMap(k -> Arrays.asList(k.split(" ")).iterator())
+                .mapToPair(k -> new Tuple2<>(k, 1))
+                .reduceByKey((k1, k2) -> k1 + k2)
+                .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2));
 
         sc.close();
     }
