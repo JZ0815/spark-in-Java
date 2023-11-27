ce3f01d784f931e0e702e25f88e1b2b7d3e412b5
Just Coding <jzhu@github.com>
2023-11-27 10:45:18 -0500

    test

--- src/main/java/WordsCount.java
+++ src/main/java/WordsCount.java
@@ -3,10 +3,11 @@ import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 
 import java.util.Arrays;
-
+import java.util.Scanner;
 
 
 public class WordsCount {
@@ -15,6 +16,16 @@ public class WordsCount {
         Logger.getLogger("org.apache").setLevel(Level.WARN);
         SparkConf conf = new SparkConf().setAppName("spark-in-Java").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
+
+        /*SparkSession sparkSession = SparkSession.builder().appName("TestingSparkSql")
+                .master("local[*]")
+                .config("spark.sql.warehouse.dir", "file:///c:/Apache/Spark/tmp")
+                .getOrCreate();
+
+        SparkSession sparkSession = SparkSession.builder().appName("TestingSparkSql")
+                .master("spark://192.168.19.1:7077")
+                .config("spark.sql.warehouse.dir", "file:///c:/Apache/Spark/tmp")
+                .getOrCreate();*/
         JavaRDD<String> lineRdd = sc.textFile("src/main/resources/sparkDev.txt");
         lineRdd.flatMap(k -> Arrays.asList(k.split(" ")).iterator())
                 .filter(wd -> !wd.equals(""))
@@ -26,6 +37,9 @@ public class WordsCount {
                 .sortByKey(false)  //decending order
                 .take(10)
                 .forEach(k -> System.out.println("\"" + k._2 + "\""+ " has " + k._1));
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
 
         sc.close();
     }
