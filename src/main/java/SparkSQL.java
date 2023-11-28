da8cbb4483136e8aed89413be36ff334bbae6ca8
Just Coding <jzhu@github.com>
2023-11-27 21:37:21 -0500

    filter 3 sytles

--- src/main/java/SparkSQL.java
+++ src/main/java/SparkSQL.java
@@ -1,8 +1,11 @@
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.storage.StorageLevel;
 
 public class SparkSQL {
     public static void main(String[] args) {
@@ -13,6 +16,7 @@ public class SparkSQL {
                 .getOrCreate();
         Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
         dataset.show();
+        dataset.persist(StorageLevel.MEMORY_AND_DISK());
         long count = dataset.count();
         System.out.println("There are " + count + " records.");
         Row firstRow = dataset.first();
@@ -21,6 +25,20 @@ public class SparkSQL {
 
         int yr = Integer.parseInt(firstRow.getAs("year"));
         System.out.println(yr);
+
+        Dataset<Row> filteredResult1 = dataset.filter("subject = 'Math'");
+        filteredResult1.show();
+
+        Dataset<Row> filteredResult2 = dataset.filter((FilterFunction<Row>) row ->
+                Integer.parseInt(row.getAs("year")) >= 2007) ;
+        filteredResult2.show();
+
+        Column subjectColumn = dataset.col("subject");
+        Column yearColumn = dataset.col("year");
+        Dataset<Row> filteredResult3 = dataset.filter(subjectColumn.equalTo("History")
+                .and(yearColumn.geq(2007))) ;
+        filteredResult3.show();
+
 
         spark.close();
     }
